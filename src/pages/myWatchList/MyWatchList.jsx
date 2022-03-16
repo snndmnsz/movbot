d324be127b00f9f792f5124c6b534a9132cf66a2
snndmnsz <42818330+snndmnsz@users.noreply.github.com>
2022-03-16 04:00:27 +0300

    styles page added

--- src/pages/myWatchList/MyWatchList.jsx
+++ src/pages/myWatchList/MyWatchList.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-
+import React from "react";
+import styles from "./watchList.module.css";
 function MyWatchList() {
   return (
-    <div>WatchList</div>
-  )
+    <div className={styles.watchList__container}>
+      <h1 className={styles.title}>Watch List</h1>
+    </div>
+  );
 }
 
-export default MyWatchList
+export default MyWatchList;
